46c882bc3be8a91544f78299f28b11901ebcf85a
Wesley Tejeda <wtejeda@talentpath.com>
2022-02-28 00:11:26 -0500

    Error handling for purchasing funds added

--- src/app/funds/funds.component.ts
+++ src/app/funds/funds.component.ts
@@ -16,6 +16,8 @@ export class FundsComponent implements OnInit {
   loaded:boolean = false;
   loggedIn:boolean = false;
   confirmationMessage:string = "";
+  errorMessage:string = "";
+  success:boolean = false;
   constructor(private getService:GetService, private postService: PostService) { }
 
   ngOnInit(): void {
@@ -42,6 +44,9 @@ export class FundsComponent implements OnInit {
 
   selectFund(fund:Fund){
     this.selectedFund = fund;
+    this.confirmationMessage = "";
+    this.success = false;
+    this.errorMessage = "";
   }
 
   updateTotal(fund:any){
@@ -62,7 +67,11 @@ export class FundsComponent implements OnInit {
       console.log(res);
       if(!res.error){
         this.confirmationMessage = "Successfully bought "+ this.amountToBuy + " shares."
+        this.success = true;
       }
     })
+    if(this.success === false){
+      this.errorMessage = "Make sure you have enough currency in your account before purchasing this fund."
+    }
   }
 }
